e4dc9389b8f97e23eacc6ba8cdd2e581ba8e09ab
takao.oyobe <takao.oyobe@P19378.intra.rakuten.co.jp>
2013-04-20 14:10:59 +0900

    King

--- src/main/resources/js/GijiRock_read.js
+++ src/main/resources/js/GijiRock_read.js
@@ -1,9 +1,53 @@
+if ($ === undefined) {
+    var $ = jQuery;
+}
 $(function(){
+    var getStamp = function(id){
+        var stamp = "";
+        switch (id) {
+            case "st_001" :
+                stamp = "st_001.png";
+                break;
+            case "st_002" :
+                stamp = "st_002.png";
+                break;
+        }
+        return stamp;
+    }
+    var getHtmlByType = function(type, content){
+        var html = "";
+        if (type=="stamp") {
+            html = '<div id="'+content+'" />';
+        } else if (type=="audio") {
+            html = '<span class="mp3">'+content+'.mp3'+'</span>';
+        } else {
+            html = '<p>'+content+'</p>';
+        }
+        return html;
+    };
     var pageId = $("meta[name='ajs-page-id']").attr("content");
     var jsonPath = "/confluence/download/attachments/" + pageId + "/gijirock.json?api=v2";
+
+    $("#splitter-content").addClass("ui-widget-content");
     $.getJSON(jsonPath, function(json){
-        $(json).each(function(){
-            console.log(this["data"]["content"]);
+        $.each(json, function(key, value){
+            var uuid = key;
+            var html = '<div class="gijirockContent ui-widget-content" id='+uuid+'>';
+            html = html+getHtmlByType(value["data"]["type"], value["data"]["content"]);
+            html = html+'</div>';
+
+            $("#main").append(html);
+            $("#"+uuid).css("top", value["position"]["top"]+"px");
+            $("#"+uuid).css("left", value["position"]["left"]+"px");
+
+            // Dragできるようにする
+            $("#"+uuid).draggable({ containment: "#splitter-content", scroll: false });
+            $("#"+uuid).mouseover(function(){
+                $("#"+uuid).css("border", "3px dotted #FF99FF");
+            }).mouseout(function(){
+                $("#"+uuid).css("border", "");
+            });
+
         });
     });
 });
